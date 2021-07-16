2dc9ae58c32344a4a92a6e92ac19bb64e8adb1d2
anshul goyal <anshulgoyal211@gmail.com>
2021-07-16 23:20:56 +0530

    some changes

--- MVC/SRC/VIEW/studentInfo.js
+++ MVC/SRC/VIEW/studentInfo.js
@@ -1,7 +1,7 @@
 init.view.studentInfo={
     setupUI:function(){
         var dom=document.getElementById("studentInfo")
-        const stu=new Student('ansh','gupta',23)
+        const stu=new Student('anshul','goyal',23)
         
         var stud=stu.printStudent()
        dom.innerHTML=JSON.stringify(stud);
